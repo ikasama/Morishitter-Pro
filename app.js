f7f62ff68ce5daa29c8143b0fcec83404a83113d
Masaaki Morishita <ikasama3.0@gmail.com>
2013-10-14 21:33:35 +0900

    Implement login by twitter

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var OAuth = require('oauth').OAuth;
 
 var app = express();
 
@@ -19,6 +20,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+app.use(express.cookieParser('F8de9f86opD74M'));
+app.use(express.session());
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,4 +37,52 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+var oa = new OAuth(
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  "BLXRg0QkrJrWJsnfptMw",
+  "9Zt4qjzwanf5HLrBSGWBkbi82Un10H5UfR5LT315CsI",
+  "1.1",
+  "http://127.0.0.1:3000/auth/twitter/callback",
+  "HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res){
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+    if (error) {
+      console.log(error);
+      res.send("yeah no. didn't work.");
+    } else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      console.log('oauth.token: ' + req.session.oauth.token);
+      req.session.oauth.token_secret = oauth_token_secret;
+      console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token);
+    }
+  });
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth = req.session.oauth;
+    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+    function(error, oauth_access_token, oauth_access_token_secret, results){
+      if (error){
+        console.log(error);
+        res.send("yeah something broke.");
+      } else {
+        req.session.oauth.access_token = oauth_access_token;
+        req.session.oauth.access_token_secret = oauth_access_token_secret;
+        console.log(results);
+        res.send("worked. nice one.");
+      }
+    });
+  } else {
+    next(new Error("you're not supposed to be here."));
+  }
 });
